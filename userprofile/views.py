6839bf081ff7772c01fa66443d2f18945919fd49
HY\harry <hy546880109@qq.com>
2022-09-26 18:37:45 +0800

    用户登录和登出

--- userprofile/views.py
+++ userprofile/views.py
@@ -5,17 +5,17 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from .forms import UserLoginForm
+from userprofile.forms import UserLoginForm
 
 
-def user_login(reuqest):
+def user_login(request):
     if request.method == 'POST':
         user_login_form = UserLoginForm(data=request.POST)
         if user_login_form.is_valid():
             data = user_login_form.cleaned_data
             user = authenticate(username=data['username'], password=data['password'])
             if user:
-                login(requests,user)
+                login(request,user)
                 return redirect('article:article_list')
             else:
                 return HttpResponse('账号密码输入有误')
@@ -24,6 +24,6 @@ def user_login(reuqest):
     elif request.method == 'GET':
         user_login_form = UserLoginForm()
         context = {'form':user_login_form}
-        return render(request, 'userprofiel/login.html', context)
+        return render(request, 'userprofile/login.html', context)
     else:
         return HttpResponse('请使用post或者get')
